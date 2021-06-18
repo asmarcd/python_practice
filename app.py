94d1672059f3219a51364b04f598668a3303f968
asmarcd <asmarcd@gmail.com>
2021-06-17 20:03:57 -0700

    variables

---
+++ app.py
@@ -0,0 +1,11 @@
+character_name = "John"
+character_age = "35"
+is_male = True
+
+print("There once was a man named " + character_name + ",")
+print("He was " + character_age + " years old")
+
+character_name = "Tim"
+
+print("He liked the name " + character_name)
+print("He didn't like being " + character_age)
