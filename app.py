3a4da14f7c432262fd3f5fdf3113dd7066934e11
asmarcd <asmarcd@gmail.com>
2021-06-17 20:32:15 -0700

    float and int

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-character_name = "John"
-character_age = "35"
-is_male = True
-
-print("There once was a man named " + character_name + ",")
-print("He was " + character_age + " years old")
-
-character_name = "Tim"
-
-print("He liked the name " + character_name)
-print("He didn't like being " + character_age)
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
