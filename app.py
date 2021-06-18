4e2247dfc292d92597028419e67c411e79e60cc0
asmarcd <asmarcd@gmail.com>
2021-06-18 15:03:08 -0700

    new file creation

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-employee_file = open('employees.txt', 'a')
-employee_file.write('\nKelly - Customer Service')
+employee_file = open('index.html', 'w')
+employee_file.write('<p>This is HTML</p>')
 employee_file.close()
